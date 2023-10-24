2c6b767ae682f6498758d58e48aeaf07d9cf93e2
NukaCat <sqrt19@gmail.com>
2023-10-25 01:09:38 +0400

    add video select

--- client/main.js
+++ client/main.js
@@ -1,8 +1,16 @@
 
-function fetch_subs_for_video(video_name) {
-  const name_without_ext = video_name.substring(0, video_name.lastIndexOf('.'))
-  const sub_name = name_without_ext + '.srt'
-  return fetch('api/get_sub/' + sub_name).then((response) => {
+function fetch_subs_for_video(title, video_name) {
+  return fetch(`api/sub/${title}/${video_name}`).then((response) => {
+      if(!response.ok) {
+        console.log('Response error', response)
+      }
+      return response.json()
+  })
+}
+
+
+function fetch_video_list() {
+  return fetch('api/video_list').then((response) => {
       if(!response.ok) {
         console.log('Response error', response)
       }
@@ -59,10 +67,8 @@ class SubPanel {
 class VideoPlayer {
   constructor(video_element) {
     this.element = video_element
-  }
-  
-  set on_video_update(on_video_update_cb) {
-    this.element.onTimeUpdate = on_video_update_cb
+    this.on_video_update = () => {}
+    this.element.onTimeUpdate = () => this.on_video_update()
   }
 
   get time_ms() {
@@ -73,10 +79,10 @@ class VideoPlayer {
     this.element.currentTime = time_ms / 1000.0
   }
 
-  set_video(video_name) {
+  set_video(title, video_name) {
     const video_source_el = this.element.querySelector('#video_source')
-    video_source_el.src = 'data/' + video_name
-    video_source_el.type='video/mp4'
+    video_source_el.src = `api/video/${title}/${video_name}`
+    video_source_el.type = 'video/mp4'
     this.element.load()
   }
   
@@ -91,25 +97,43 @@ class VideoPlayer {
 }
 
 
+class TitleSelector {
+  constructor(element) {
+    this.element = element
+    this.on_video_click = (title, video_name) => {}
+  }
+  
+  request_title_list_update() {
+    fetch_video_list().then((video_list) => {
+      for(let idx = 0; idx < video_list.length; idx++) {
+        const title = video_list[idx].title
+        const video_name = video_list[idx].video_name
+
+        const title_el = document.createElement('div')
+        title_el.className = 'title'
+        title_el.innerText = video_name
+        title_el.onclick = () => this.on_video_click(title, video_name)
+        this.element.appendChild(title_el)
+      }
+    })
+  }
+}
+
+
 function main() {
   let cur_subs = null
 
-  const video_name = 'k-on/k-on1.mp4'
   const video_width = 1366
   const video_height = 768
 
   const sub_panel = new SubPanel(document.getElementById('sub_panel'))
   const video_player = new VideoPlayer(document.getElementById('video'))
+  const title_selector = new TitleSelector(document.getElementById('title_selector'))
   
-  video_player.set_video('k-on/k-on1.mp4')
   video_player.set_size(video_width, video_height)
   
   sub_panel.set_height(video_height)
 
-  fetch_subs_for_video(video_name).then((subs) => {
-    cur_subs = subs
-    sub_panel.set_subs(subs)
-  })
   
   video_player.on_video_update = function(e) {
     if(cur_subs == null) {
@@ -131,6 +155,17 @@ function main() {
     video_player.time_ms = cur_subs[sub_idx].start
     video_player.play()
   }
+
+  title_selector.on_video_click = (title, video_name) => {
+    video_player.set_video(title, video_name)
+
+    fetch_subs_for_video(title, video_name).then((subs) => {
+      cur_subs = subs
+      sub_panel.set_subs(subs)
+    })
+  }
+
+  title_selector.request_title_list_update()
 }
 
 window.onload = main
--- server/main.py
+++ server/main.py
@@ -8,18 +8,59 @@ DATA_PATH = ''
 
 app = Flask('LangVid', static_url_path='', static_folder='client')
 
-@app.route('/data/<path:path>')
-def data(path):
-    return send_from_directory(DATA_PATH, path)
-
-
 @app.route('/')
 def root():
     return send_from_directory('client', 'index.html')
 
-@app.route('/api/get_sub/<path:path>')
+@app.route('/api/video/<path:path>')
+def data(path):
+    if len(path.split('/')) != 2:
+        return app.response_class(status=400)
+
+    title, video_name = path.split('/')
+    video_path = title + '/' + video_name + ".mp4"
+
+    return send_from_directory(DATA_PATH, video_path)
+
+
+@app.route('/api/video_list')
+def get_titles():
+    videos = []
+
+    for dir_entry in os.listdir(DATA_PATH):
+        path = os.path.join(DATA_PATH, dir_entry)
+        if os.path.isfile(path):
+            name, ext = os.path.splitext(dir_entry)
+            if ext == '.mp4':
+                videos.append({
+                    "title": dir_entry,
+                    "video_name": name
+                })
+
+        if os.path.isdir(path):
+            for sub_dir_entry in os.listdir(path):
+                name, ext = os.path.splitext(sub_dir_entry)
+                if ext == '.mp4':
+                    videos.append({
+                        "title": dir_entry,
+                        "video_name": name,
+                    })
+
+    return app.response_class(
+        response=json.dumps(videos),
+        status=200,
+        mimetype='application/json'
+    )
+            
+
+@app.route('/api/sub/<path:path>')
 def get_sub(path):
-    path = os.path.join(DATA_PATH, path)
+    if len(path.split('/')) != 2:
+        return app.response_class(status=400)
+
+    title, video_name = path.split('/')
+
+    path = os.path.join(DATA_PATH, title, video_name) + ".srt"
     if not os.path.isfile(path):
         print(f'Cant find {path}')
         return app.response_class(status=404)
@@ -34,12 +75,11 @@ def get_sub(path):
             'index': sub.index,
         })
 
-    response = app.response_class(
+    return app.response_class(
         response=json.dumps(subs_data),
         status=200,
         mimetype='application/json'
     )
-    return response
     
 
 if __name__ == '__main__':
