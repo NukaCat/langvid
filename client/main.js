e850b985a19c78a3fc4e3e05fb9bdd204f906338
NukaCat <sqrt19@gmail.com>
2023-10-26 01:42:58 +0400

    fix subtitle selection

--- client/main.js
+++ client/main.js
@@ -38,7 +38,10 @@ class SubPanel {
       sub_box_el.className = 'sub_box'
 
       const sub_button_el = document.createElement('div')
-      sub_button_el.onclick = () =>  { this.on_sub_clicked(sub_idx) }
+      sub_button_el.onclick = () =>  { 
+        this.set_cur_sub_idx(sub_idx)
+        this.on_sub_clicked(sub_idx)
+      }
       sub_button_el.className = 'sub_button'
 
       const sub_text_el = document.createElement('div')
@@ -52,7 +55,7 @@ class SubPanel {
   }
   
   set_cur_sub_idx(sub_idx) {
-    const sub_boxes = this.element.querySelector('sub_box')
+    const sub_boxes = this.element.querySelectorAll('.sub_box')
     for(var i = 0; i < sub_boxes.length; i++) { 
       sub_boxes[i].className = `sub_box ${ i == sub_idx ? 'sub_box_selected': ''}`
     }
@@ -68,7 +71,7 @@ class VideoPlayer {
   constructor(video_element) {
     this.element = video_element
     this.on_video_update = () => {}
-    this.element.onTimeUpdate = () => this.on_video_update()
+    this.element.ontimeupdate = () => { this.on_video_update()}
   }
 
   get time_ms() {
@@ -144,16 +147,15 @@ function main() {
   video_player.set_size(video_width, video_height)
   
   sub_panel.set_height(video_height)
-
   
-  video_player.on_video_update = function(e) {
+  video_player.on_video_update = function() {
     if(cur_subs == null) {
       return
     }
-    const cur_time = video_player.time_ms()
+    const cur_time = video_player.time_ms
 
-    for(var i = 0; i < subs.length; i++) {
-      if(subs[i].start < cur_time && subs[i].end > cur_time) {
+    for(var i = 0; i < cur_subs.length; i++) {
+      if(cur_subs[i].start < cur_time && cur_subs[i].end > cur_time) {
         sub_panel.set_cur_sub_idx(i)
       }
     }
